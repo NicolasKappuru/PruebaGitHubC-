99f1bffe3b7de8f55614e60e4ed10e890953fdb7
NicolasKappuru <nicolascr333@gmail.com>
2025-02-19 20:42:18 -0500

    Cargo el .cpp de las Operaciones, viene incluido la suma

---
+++ Operaciones.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+	
+	//La operacion de suma
+	cout << "Hola, aqui esta una suma"<<endl;
+	
+	int a = 9; 
+	int b = 8; 
+	
+	cout << "Resultado: " << a+b <<; 
+	
+	//Operaciones siguientes
+	
+	return 0; 
+}
